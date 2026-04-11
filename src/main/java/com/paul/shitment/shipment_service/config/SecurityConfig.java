552a5507d14566a8860f536246026f3fb526b8e2
Paul <paulrodrigoguasace@gmail.com>
2026-04-10 23:40:33 -0400

    fix: add netlify origin to cors configuration

--- src/main/java/com/paul/shitment/shipment_service/config/SecurityConfig.java
+++ src/main/java/com/paul/shitment/shipment_service/config/SecurityConfig.java
@@ -99,7 +99,8 @@ public class SecurityConfig {
                 config.setAllowedOrigins(List.of(
                                 "http://localhost:3000",
                                 "http://localhost:5173",
-                                "http://localhost:4200"));
+                                "http://localhost:4200",
+                                "https://guileless-gumdrop-0b2da7.netlify.app"));
 
                 // métodos HTTP permitidos
                 config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
